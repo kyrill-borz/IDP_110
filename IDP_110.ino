5e1b29fe6bb75784c64862c6160e2be40f7ded14
kyrill <kyril.borzenko@gmail.com>
2024-01-23 11:28:41 +0000

    P Control

--- IDP_110.ino
+++ IDP_110.ino
@@ -4,23 +4,35 @@ Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 
 int leftlinesensorPin = 12;
 int rightlinesensorPin = 11; // Connect sensor to input pin 3
-Adafruit_DCMotor *LeftMotor = AFMS.getMotor(1);
+ 
+ Adafruit_DCMotor *LeftMotor = AFMS.getMotor(1);
 // You can also make another motor on port M2
-Adafruit_DCMotor *RightMotor = AFMS.getMotor(2);
+  Adafruit_DCMotor *RightMotor = AFMS.getMotor(2);
+  
 
 void setup() {
+ 
+  if (!AFMS.begin()) {         // create with the default frequency 1.6KHz
+  // if (!AFMS.begin(1000)) {  // OR with a different frequency, say 1KHz
+    Serial.println("Could not find Motor Shield. Check wiring.");
+    while (1);
+  }
  Serial.begin(9600); // Init the serial port
  pinMode(leftlinesensorPin, INPUT); // declare LED as output
  pinMode(rightlinesensorPin, INPUT); // declare Micro switch as input
-
 }
 void loop(){
  int valLeft = digitalRead(leftlinesensorPin); // read left input value
- RightMotor->run(FORWARD);
-    RightMotor->setSpeed(valLeft);
+ Serial.print(valLeft);
+ RightMotor->run(BACKWARD);
+    RightMotor->setSpeed(valLeft*200);
+    delay(10);
+
 
  int valRight = digitalRead(rightlinesensorPin); // read right input value
+ Serial.print(valRight);
     LeftMotor->run(FORWARD);
-        LeftMotor->setSpeed(valRight);
+        LeftMotor->setSpeed(valRight*200);
+        delay(10);
  delay(100);
 }
