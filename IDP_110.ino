f980130b3cf996b692f12aefe2f2ff875629e699
kyrill <kyril.borzenko@gmail.com>
2024-01-23 10:51:51 +0000

    make basic line follower loop

--- IDP_110.ino
+++ IDP_110.ino
@@ -1,14 +1,26 @@
+#include <Adafruit_MotorShield.h>
+
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+
 int leftlinesensorPin = 12;
 int rightlinesensorPin = 11; // Connect sensor to input pin 3
+Adafruit_DCMotor *LeftMotor = AFMS.getMotor(1);
+// You can also make another motor on port M2
+Adafruit_DCMotor *RightMotor = AFMS.getMotor(2);
+
 void setup() {
  Serial.begin(9600); // Init the serial port
  pinMode(leftlinesensorPin, INPUT); // declare LED as output
  pinMode(rightlinesensorPin, INPUT); // declare Micro switch as input
+
 }
 void loop(){
  int valLeft = digitalRead(leftlinesensorPin); // read left input value
- Serial.print(valLeft);
+ RightMotor->run(FORWARD);
+    RightMotor->setSpeed(valLeft);
+
  int valRight = digitalRead(rightlinesensorPin); // read right input value
- Serial.println(valRight);
+    LeftMotor->run(FORWARD);
+        LeftMotor->setSpeed(valRight);
  delay(100);
 }
