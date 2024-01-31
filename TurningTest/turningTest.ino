dc75e16bb45185350484d70c5f1c3cfa8cc0795b
jl2322 <jl2322@cam.ac.uk>
2024-01-31 10:16:12 +0000

    turning

--- TurningTest/turningTest.ino
+++ TurningTest/turningTest.ino
@@ -28,35 +28,36 @@ void setup() {
  pinMode(leftForward, INPUT);
  pinMode(rightForward, INPUT);
 
- attachInterrupt(digitalPinToInterrupt(leftForward),stopRightTurn,FALLING); //interrupts triggered by front line sensors to stop turning
- attachInterrupt(digitalPinToInterrupt(rightForward),stopLeftTurn,FALLING);
+ //attachInterrupt(digitalPinToInterrupt(leftForward),stopRightTurn,FALLING); //interrupts triggered by front line sensors to stop turning
+ //attachInterrupt(digitalPinToInterrupt(rightForward),stopLeftTurn,FALLING);
 
 }
 
-void turnLeft(){ //adjust turning functions to match motor orientations
-  turningLeft = 1;
-  while(turningLeft == 1){
-    RightMotor->run(FORWARD);
-    RightMotor->setSpeed(200);
-    LeftMotor->run(BACKWARD);
-    LeftMotor->setSpeed(200);
-  }
+void turnLeft(){//adjust turning functions to match motor orientations
+ RightMotor->run(BACKWARD);
+ RightMotor->setSpeed(200);
+ LeftMotor->run(BACKWARD);
+ LeftMotor->setSpeed(200);
+ delay(2000);
+ RightMotor->setSpeed(0);
+ LeftMotor->setSpeed(0);
+ delay(2000);
 }
-
 void turnRight(){
-  turningRight = 1;
-  while(turningLeft == 1){
-    LeftMotor->run(FORWARD);
-    LeftMotor->setSpeed(200);
-    RightMotor->run(BACKWARD);
-    RightMotor->setSpeed(200);
-  }
+ LeftMotor->run(FORWARD);
+ LeftMotor->setSpeed(200);
+ RightMotor->run(FORWARD);
+ RightMotor->setSpeed(200);
+ delay(2000);
+ RightMotor->setSpeed(0);
+ LeftMotor->setSpeed(0);
+ delay(2000);
 }
 
-void stopLeftTurn(){  //functions called by Interrupts
-  turningLeft = 0;
+/*void stopLeftTurn(){  //functions called by Interrupts
+turningLeft = 0;
 }
 
 void stopRightTurn(){
   turningRight = 0;
-}
+} */
