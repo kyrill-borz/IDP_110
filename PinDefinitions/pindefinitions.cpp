65abac514c2cd46625c761413709afd29082a104
kyrill <kyril.borzenko@gmail.com>
2024-02-01 19:13:56 +0000

    change file structure of code

---
+++ PinDefinitions/pindefinitions.cpp
@@ -0,0 +1,7 @@
+// pin definitions
+int leftlinesensorPin = 9;
+int rightlinesensorPin = 8; 
+int leftjunctionsensorPin = 10;
+int rightjunctionsensorPin = 7;
+int crashswitchPin = 3; 
+int pushButton = 5;
---
+++ PinDefinitions/pindefinitions.h
@@ -0,0 +1,11 @@
+#ifndef PINDEFINITIONS_H
+#define PINDEFINITIONS_H
+
+extern int leftlinesensorPin;
+extern int rightlinesensorPin; 
+extern int leftjunctionsensorPin;
+extern int rightjunctionsensorPin;
+extern int crashswitchPin; 
+extern int pushButton;
+
+#endif
