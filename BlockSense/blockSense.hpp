b6b1c1aa1cf9c0000105f943833fa1afc96496ad
kyrill <kyril.borzenko@gmail.com>
2024-02-07 15:29:50 +0000

    block sensing

--- BlockSense/blockSense.hpp
+++ BlockSense/blockSense.hpp
@@ -13,12 +13,12 @@ bool senseBlockIR(DFRobot_VL53L0X sensor){ //sensing block distance using IR
 }
 
 
-void blockTypeIR(DFRobot_VL53L0X sensor){//determing block type using IR
+int blockTypeIR(DFRobot_VL53L0X sensor ){//determing block type using IR
     float blockTypeDist = sensor.getDistance();
     if(blockTypeDist < 60){ //calibrate threshold for block identification
-        isFoam = 0; //return 0 for a solid block
+        return 0; //return 0 for a solid block
     }
     else{
-        isFoam = 1; //return 1 for a foam block
+        return 1; //return 1 for a foam block
     }
 }
